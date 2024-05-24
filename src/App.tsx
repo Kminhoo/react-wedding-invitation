9971c5fe00abf547bbfde7ba4652c34198bc3e14
minhoo <whdgus4158@gmail.com>
2024-05-25 01:55:14 +0900

    feat: Firebase 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from 'firebaseApp'
 
 const App = () => {
-  return <div>App</div>
+  const [wedding, setWedding] = useState<any>()
+
+  const getWeddingData = async () => {
+    const docRef = doc(db, 'wedding', 'v3eNky8xXowBjARen4i5')
+    const docSnap = await getDoc(docRef)
+
+    console.log(docSnap.data())
+    setWedding(docSnap.data())
+  }
+
+  useEffect(() => {
+    getWeddingData()
+  }, [])
+
+  return <div>{JSON.stringify(wedding)}</div>
 }
 
 export default App
---
+++ src/firebaseApp.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, getApp, initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+export let app: FirebaseApp
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+}
+
+try {
+  app = getApp('app')
+} catch (error) {
+  app = initializeApp(firebaseConfig, 'app')
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
+
+export default firebaseApp
