1f6e4347f6b1859d36e8cae081189036e142aa43
kminhoo <whdgus4158@gmail.com>
2024-05-26 20:32:31 +0900

    feat: Bgm 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from 'firebaseApp'
 
+import Bgm from 'components/Bgm'
+
 const App = () => {
+  const [loading, setLoading] = useState<Boolean>(false)
+
+  const [error, setError] = useState()
+
   const [wedding, setWedding] = useState<any>()
 
   const getWeddingData = async () => {
-    const docRef = doc(db, 'wedding', 'v3eNky8xXowBjARen4i5')
-    const docSnap = await getDoc(docRef)
+    try {
+      setLoading(true)
 
-    console.log(docSnap.data())
-    setWedding(docSnap.data())
+      const docRef = doc(db, 'wedding', 'v3eNky8xXowBjARen4i5')
+      const docSnap = await getDoc(docRef)
+
+      setWedding(docSnap.data())
+      setLoading(false)
+    } catch (error: any) {
+      setError(error)
+    }
   }
 
   useEffect(() => {
     getWeddingData()
   }, [])
 
-  return <div>{JSON.stringify(wedding)}</div>
+  if (loading) {
+    return <div>로딩중</div>
+  }
+
+  if (error) {
+    return <div>에러발생!</div>
+  }
+
+  return (
+    <div className="App">
+      <Bgm />
+      {JSON.stringify(wedding)}
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Bgm.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useState } from 'react'
+
+const Bgm = () => {
+  const [playMusic, setPlayMusic] = useState<Boolean>(false)
+
+  const ref = useRef<HTMLAudioElement>(null)
+
+  const playbgm = () => {
+    setPlayMusic(true)
+    ref.current?.play()
+  }
+
+  const stopBgm = () => {
+    setPlayMusic(false)
+    ref.current?.pause()
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ref.current && ref.current.paused) {
+        ref.current.play()
+        setPlayMusic(true)
+
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <div className="bgm" onClick={playMusic ? stopBgm : playbgm}>
+      <audio
+        ref={ref}
+        loop={true}
+        autoPlay={true}
+        src="./assets/background.mp3"
+      />
+      {playMusic ? <StopMusic /> : <PlayMusic />}
+    </div>
+  )
+}
+
+export default Bgm
+
+const StopMusic = () => {
+  return (
+    <svg className="bgm__icon" viewBox="0 0 32 32">
+      <title />
+      <g id="Pause">
+        <path d="M16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2Zm0,26A12,12,0,1,1,28,16,12,12,0,0,1,16,28Z" />
+        <path d="M13,11a1,1,0,0,0-1,1v8a1,1,0,0,0,2,0V12A1,1,0,0,0,13,11Z" />
+        <path d="M19,11a1,1,0,0,0-1,1v8a1,1,0,0,0,2,0V12A1,1,0,0,0,19,11Z" />
+      </g>
+    </svg>
+  )
+}
+
+const PlayMusic = () => {
+  return (
+    <svg className="bgm__icon" viewBox="0 0 32 32">
+      <title />
+      <g id="Play">
+        <path d="M16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2Zm0,26A12,12,0,1,1,28,16,12,12,0,0,1,16,28Z" />
+        <path d="M22.5,15.13l-9-5.2a1,1,0,0,0-1.5.87V21.2a1,1,0,0,0,1.5.87l9-5.2a1,1,0,0,0,0-1.73ZM14,19.46V12.54L20,16Z" />
+      </g>
+    </svg>
+  )
+}
