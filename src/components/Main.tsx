3e5e2a4ad1d03f97c2d6a4065c4667211e19be7e
kminhoo <whdgus4158@gmail.com>
2025-02-23 20:21:20 +0900

    feat: LCP 이미지 preload 및 Navigation 컴포넌트 lazy 적용

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,20 @@
+import { Suspense, lazy } from 'react'
 import Hero from '@sections/Hero'
 import Info from '@sections/Info'
 import Invitation from '@sections/Invitation'
 import Calendar from '@sections/Calendar'
 import ImageGallery from '@sections/ImageGallery'
 import Locations from '@sections/Locations'
-import Navigation from '@sections/Navigation'
+// Navigation 임포트 제거
 import WayToCome from '@sections/WayToCome'
 import GuestBook from '@sections/GuestBook'
 import Account from '@sections/Account'
 import Rsvp from '@sections/Rsvp'
 
 import { Wedding } from 'models/wedding'
+
+// Navigation을 lazy로 임포트
+const Navigation = lazy(() => import('@sections/Navigation'))
 
 const Main = ({ wedding }: { wedding: Wedding }) => {
   const {
@@ -46,7 +50,9 @@ const Main = ({ wedding }: { wedding: Wedding }) => {
       <ImageGallery images={galleryImages} />
       <Calendar date={date} />
       <Locations location={location} />
-      <Navigation link={location.link} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navigation link={location.link} />
+      </Suspense>
       <WayToCome location={location} />
       <GuestBook />
       <Account message={accountMessage} groom={groom} bride={bride} />
@@ -56,3 +62,62 @@ const Main = ({ wedding }: { wedding: Wedding }) => {
 }
 
 export default Main
+
+// import Hero from '@sections/Hero'
+// import Info from '@sections/Info'
+// import Invitation from '@sections/Invitation'
+// import Calendar from '@sections/Calendar'
+// import ImageGallery from '@sections/ImageGallery'
+// import Locations from '@sections/Locations'
+// import Navigation from '@sections/Navigation'
+// import WayToCome from '@sections/WayToCome'
+// import GuestBook from '@sections/GuestBook'
+// import Account from '@sections/Account'
+// import Rsvp from '@sections/Rsvp'
+
+// import { Wedding } from 'models/wedding'
+
+// const Main = ({ wedding }: { wedding: Wedding }) => {
+//   const {
+//     date,
+//     location,
+//     groom,
+//     bride,
+//     message,
+//     galleryImages,
+//     accountMessage,
+//   } = wedding
+
+//   return (
+//     <main>
+//       <Hero />
+//       <Info
+//         date={date}
+//         locationName={location.name}
+//         groomName={groom.name}
+//         brideName={bride.name}
+//       />
+//       <Invitation
+//         message={message}
+//         groomName={groom.name}
+//         brideName={bride.name}
+//         groomTel={groom.phoneNumber}
+//         brideTel={bride.phoneNumber}
+//         groomFatherName={groom.parents[0].name}
+//         groomMotherName={groom.parents[1].name}
+//         brideFatherName={bride.parents[0].name}
+//         brideMotherName={bride.parents[1].name}
+//       />
+//       <ImageGallery images={galleryImages} />
+//       <Calendar date={date} />
+//       <Locations location={location} />
+//       <Navigation link={location.link} />
+//       <WayToCome location={location} />
+//       <GuestBook />
+//       <Account message={accountMessage} groom={groom} bride={bride} />
+//       <Rsvp />
+//     </main>
+//   )
+// }
+
+// export default Main
